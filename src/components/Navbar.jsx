66d30274b9ef6095f863c8f1ba61a6647c4cb292
Anik Saha <96070982+anik-saha-2001@users.noreply.github.com>
2024-10-16 12:58:50 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         ANIK&#39;S PORTFOLIO
       </h1>
       <a
-        href="https://drive.google.com/file/d/1xLjFR1C9Ite6dmxOM7gGnISni2dK8G5I/view"
+        href="https://drive.google.com/file/d/1nUwlEnRsqlhV4tSrzeD0jYxOQ_Oy1qhX/view?usp=sharing"
         target="_blank"
         className="font-bold text-black bg-green-500 py-2 px-3 hover:bg-green-800 hover:text-white"
       >
